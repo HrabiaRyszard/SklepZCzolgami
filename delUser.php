65a4df69c1770a4070cb19b5f592a3d3d347ad28
Lavendi-trw <shoplavendistore@gmail.com>
2025-05-21 17:14:14 +0200

    del ini

--- delUser.php
+++ delUser.php
@@ -10,11 +10,6 @@ $id = $_SESSION['userID'];
 
 $_SESSION = [];
 
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
-}
-
 session_destroy();
 
 require 'db.php';
