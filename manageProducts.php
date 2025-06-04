a01551243ebd37827484634f22681d5209dfebd7
Sim1700 <kontakt@szymonlinek.pl>
2025-06-04 10:36:43 +0200

    Update manageProducts.php

--- manageProducts.php
+++ manageProducts.php
@@ -17,54 +17,34 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
 </head>
 
 <body>
-    <header>
-        <a href="index.php">
-            <h1 class="noMargin">Sklep ogrodniczy</h1>
-        </a>
-        <div class="hOptions">
-            <a href="products.php">Sklep</a>
-            <a href="cart.php">Koszyk</a>
-        </div>
-        <div class="buttonContainer">
-            <a href="login.php"><button>Logowanie</button></a>
-            <a href="register.php"><button>Rejestracja</button></a>
-        </div>
-    </header>
-    <main>
-        <div>
-            <h1>Zarządzaj produktami</h1>
-            <table>
-                <tr>
-                    <th>Zdjęcie</th>
-                    <th>Nazwa produktu</th>
-                    <th>Opis</th>
-                    <th>Cena</th>
-                    <th>Kategoria</th>
-                    <th>Akcje</th>
-                </tr>
-                <?php
-                $sql = "SELECT produkt.*, kategoria.nazwa AS kategoria_nazwa 
-                FROM produkt 
-                LEFT JOIN kategoria ON produkt.kategoria_id = kategoria.id";
+    <h1>Zarządzaj produktami</h1>
+    <table>
+        <tr>
+            <th>Zdjęcie</th>
+            <th>Nazwa produktu</th>
+            <th>Opis</th>
+            <th>Cena</th>
+            <th>Kategoria</th>
+            <th>Akcje</th>
+        </tr>
+        <?php
+        $sql = "SELECT produkt.*, kategoria.nazwa AS kategoria_nazwa 
+        FROM produkt 
+        LEFT JOIN kategoria ON produkt.kategoria_id = kategoria.id";
 
-                $result = mysqli_query($db, $sql);
-                while ($row = mysqli_fetch_assoc($result)) {
-                    echo "<tr>";
-                    echo "<td><img class='productImage' src='images/" . $row['url_zdjecia'] . "' alt='zdjecie' class='imgProduct'></td>";
-                    echo "<td>" . $row['nazwa'] . "</td>";
-                    echo "<td>" . $row['opis'] . "</td>";
-                    echo "<td>" . $row['cena'] . "</td>";
-                    echo "<td>" . $row['kategoria_nazwa'] . "</td>";
-                    echo "<td><a href='modifyProduct.php?id=" . $row['id'] . "'><button class='modifyBtn'>Modyfikuj</button></a></td>";
-                    echo "</tr>";
-                }
-                ?>
-            </table>
-        </div>
-    </main>
-    <footer>
-        Autorzy: <b>Ryszard Osiński</b>, <b>Mirosław Karpowicz</b>, <b>Szymon Linek</b>, <b>Krystian Kotowski</b>
-    </footer>
+        $result = mysqli_query($db, $sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            echo "<td><img class='productImage' src='images/" . $row['url_zdjecia'] . "' alt='zdjecie' class='imgProduct'></td>";
+            echo "<td>" . $row['nazwa'] . "</td>";
+            echo "<td>" . $row['opis'] . "</td>";
+            echo "<td>" . $row['cena'] . "</td>";
+            echo "<td>" . $row['kategoria_nazwa'] . "</td>";
+            echo "<td><a href='modifyProduct.php?id=" . $row['id'] . "'><button class='modifyBtn'>Modyfikuj</button></a></td>";
+            echo "</tr>";
+        }
+        ?>
+    </table>
 </body>
 
 </html>
