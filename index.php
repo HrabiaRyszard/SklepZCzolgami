5b0160d468c8b326cb16bcbd9ab344a3b38da6cb
Lavendi-trw <shoplavendistore@gmail.com>
2025-06-08 22:46:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,11 +9,17 @@
 <body>
     <header>
         <a href="index.php"><h1 class="noMargin">Sklep ogrodniczy</h1></a>
-        <div class="hOptions">
-            <a href="products.php">Sklep</a>
-            <a href="cart.php">Koszyk</a>
-        </div>
         <div class="buttonContainer">
+            <a href="products.php">
+                <button class="iconButton">
+                    <img src="./icons/products.svg" alt="Produkty" style="width:48px; height:48px; vertical-align:middle;">
+                </button>
+            </a>
+            <a href="cart.php">
+                <button class="iconButton">
+                    <img src="./icons/cart.svg" alt="Produkty" style="width:48px; height:48px; vertical-align:middle;">
+                </button>
+            </a>
             <a href="login.php" >
                 <button class="iconButton">
                     <img src="./icons/account.svg" alt="Konto" style="width:48px; height:48px; vertical-align:middle;">
